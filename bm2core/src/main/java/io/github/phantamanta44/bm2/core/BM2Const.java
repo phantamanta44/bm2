a3c7b500b426be0e43042b1beb5006eb71d87319
Phanta <evandalong@gmail.com>
2015-12-27 11:05:45 -0600

    Bump up version numbers

--- bm2core/src/main/java/io/github/phantamanta44/bm2/core/BM2Const.java
+++ bm2core/src/main/java/io/github/phantamanta44/bm2/core/BM2Const.java
@@ -5,7 +5,7 @@ import io.github.phantamanta44.bm2.core.util.BM2Version;
 public class BM2Const {
 	
 	public static final String MOD_ID = "brawlmodularmod";
-	public static final String MOD_VERS_STR = "1.0.0";
+	public static final String MOD_VERS_STR = "1.0.1";
 	public static final BM2Version MOD_VERS = new BM2Version(MOD_VERS_STR);
 	
 }
