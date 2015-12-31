f98d41aa657fc0d36cb9527e05d4f9a09f15973e
Phanta <evandalong@gmail.com>
2015-12-31 13:13:13 -0600

    Check if it's a brawl server before dispatching a commandswallower

--- bm2core/src/main/java/io/github/phantamanta44/bm2/core/BrawlServer.java
+++ bm2core/src/main/java/io/github/phantamanta44/bm2/core/BrawlServer.java
@@ -1,5 +1,7 @@
 package io.github.phantamanta44.bm2.core;
 
+import java.util.Arrays;
+
 public enum BrawlServer {
 
 	LOBBY("lobby", "lobby.brawl.com"),
@@ -16,7 +18,10 @@ public enum BrawlServer {
 	KITPVP("kitpvp", "kitbrawl.com"),
 	EVENT("event", "event.brawl.com");
 	
-	private static final String KEY_ROOT = "bm2.server.";
+	private static final String KEY_ROOT = "bm2.server.", REGEX_ROOT = "(\\d+\\.)?";
+	private static final String[] DIRECT_IPS = new String[] {
+		"192.99.150.162", "192.99.46.105", "192.99.150.141", "192.99.21.112"
+	};
 	
 	public final String name;
 	public final String ipRoot;
@@ -31,7 +36,15 @@ public enum BrawlServer {
 	}
 	
 	public String getIpRegex() {
-		return "\\d+\\." + this.ipRoot.replaceAll("\\.", "\\.");
+		return REGEX_ROOT + this.ipRoot.replaceAll("\\.", "\\.");
+	}
+
+	public static boolean isBrawlServer(String ip) {
+		return ip.matches("(\\S+\\.)?brawl\\.com")
+			|| Arrays.stream(values())
+			.anyMatch(s -> ip.matches(s.getIpRegex()))
+			|| Arrays.stream(DIRECT_IPS)
+			.anyMatch(i -> ip.trim().equalsIgnoreCase(i));
 	}
 	
 }
--- bm2core/src/main/java/io/github/phantamanta44/bm2/core/event/world/WorldChangeListener.java
+++ bm2core/src/main/java/io/github/phantamanta44/bm2/core/event/world/WorldChangeListener.java
@@ -1,5 +1,6 @@
 package io.github.phantamanta44.bm2.core.event.world;
 
+import io.github.phantamanta44.bm2.core.BrawlServer;
 import io.github.phantamanta44.bm2.core.event.chat.CommandSwallower;
 import io.github.phantamanta44.bm2.core.module.ModuleManager;
 import net.minecraft.client.Minecraft;
@@ -12,12 +13,17 @@ public class WorldChangeListener {
 	
 	@SubscribeEvent
 	public void onWorldJoin(EntityJoinWorldEvent event) {
-		if (!checkPending && event.entity.equals(Minecraft.getMinecraft().thePlayer)) {
-			checkPending = true;
-			new CommandSwallower("/ip", "^Server Address: (\\d+\\.)?(\\S*\\.)+\\w{2,4}$").promise(response -> {
-				checkPending = false;
-				ModuleManager.ipUpdate(response[0].substring(16));				
-			}).dispatch();
+		Minecraft mc = Minecraft.getMinecraft();
+		if (!checkPending && event.entity.equals(mc.thePlayer)) {
+			if (!BrawlServer.isBrawlServer(mc.getCurrentServerData().serverIP))
+				ModuleManager.ipUpdate(mc.getCurrentServerData().serverIP);
+			else {
+				checkPending = true;
+				new CommandSwallower("/ip", "^Server Address: (\\d+\\.)?(\\S*\\.)+\\w{2,4}$").promise(response -> {
+					checkPending = false;
+					ModuleManager.ipUpdate(response[0].substring(16));				
+				}).dispatch();
+			}
 		}
 	}
 	
